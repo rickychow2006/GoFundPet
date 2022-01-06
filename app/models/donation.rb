9c52313c6d32b5613d0925c2edcfe12efc8b0773
rickychow2006 <rickychow0716@gmail.com>
2022-01-06 11:17:33 -0600

    set up models for pet, donation, and user

---
+++ app/models/donation.rb
@@ -0,0 +1,6 @@
+class Donation < ApplicationRecord
+  belongs_to :pet
+  belongs_to :user 
+
+  validates :amount, :message, presence:true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_many :pets
+	has_many :donations
+
   has_secure_password 
+	validates :username, :password, presence: true
   validates :username, uniqueness: true
+
 end
