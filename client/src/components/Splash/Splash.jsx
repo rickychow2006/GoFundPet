a27f2abbe788692276d0865c39191c6723eae7e8
rickychow2006 <rickychow0716@gmail.com>
2022-01-08 11:49:05 -0600

    completed splash page

--- client/src/components/Splash/Splash.jsx
+++ client/src/components/Splash/Splash.jsx
@@ -21,7 +21,7 @@ class Splash extends React.Component {
           </div>
           <div className ="splashpage-first-illuistration">
             <div className ="img-container">
-              <img src="https://imagecdn.mightycause.com/d900a6df-85c6-4ca8-83ab-b19f17f0fa12/-/format/auto/-/progressive/yes/-/stretch/off/-/quality/smart/-/resize/526x/" width="600" height="400" alt="header_picture">
+              <img src="https://imagecdn.mightycause.com/d900a6df-85c6-4ca8-83ab-b19f17f0fa12/-/format/auto/-/progressive/yes/-/stretch/off/-/quality/smart/-/resize/526x/" width="600" height="380" alt="header_picture">
               </img>
             </div>
           </div>
@@ -32,10 +32,28 @@ class Splash extends React.Component {
           </h1>
         </div>
         <div>
+          <br/>
         <h4 className="splashpage-third-text">
-            For most of us, our pets are beloved family members. When your pet is sick or hurt and in need of veterinary care, you want to provide the best care possible for them. Unfortunately routine veterinary care can cost hundreds of dollars and you could be saddled with a vet bill for thousands of dollars if your pet suffers a health emergency. That’s where GoFundPet comes in.
-          </h4>
-
+          For most of us, our pets are beloved family members. When your pet is sick or hurt and in need of veterinary care, you want to provide the best care possible for them. Unfortunately routine veterinary care can cost hundreds of dollars and you could be saddled with a vet bill for thousands of dollars if your pet suffers a health emergency. That’s where GoFundPet comes in.
+        </h4>
+          <br/>
+        </div>
+        <div className="splashpage-second">
+          <h1 className="splashpage-second-title">
+          Make a difference today
+          </h1>
+        </div>
+        <div>
+          <br/>
+        <h4 className="splashpage-third-text">
+        Caring for our furry family members sometimes costs more than we can afford alone, but rarely more than we can afford together. With CoFundPet, you give donors confidence that their money is being used ONLY for your pet’s veterinary expenses. Let us help save your pet’s life by getting you needed help paying vet bills!
+        </h4>
+          <br/>
+        </div>
+        <div className="splashpage-second">
+          <h1 className="splashpage-second-title">
+          Kindness + Love
+          </h1>
         </div>
     </div>
     );
