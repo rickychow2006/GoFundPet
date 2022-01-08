ade913d543aa96467662b856f0ef639c028079f8
rickychow2006 <rickychow0716@gmail.com>
2022-01-08 11:48:36 -0600

    updated index.js and styling

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = {store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
