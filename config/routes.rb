e93187f46863c44be9ad10d3a8b2c8ccf461744a
rickychow2006 <rickychow0716@gmail.com>
2022-01-08 11:49:26 -0600

    updated routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do 
-    resources :users, only: [:index, :show]
+    resources :donations
+    resources :pets
+    resources :users
   end 
 
   mount ActionCable.server, at: '/cable'
